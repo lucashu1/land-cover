10f7af14b822851f2be378cfabe6213136f1e128
Lucas Hu <lucashu@usc.edu>
2020-02-19 23:59:33 -0800

    remove resnet

--- classify.py
+++ classify.py
@@ -1,9 +1,9 @@
 '''
 Author: Lucas Hu (lucashu@usc.edu)
-Timestamp: Fall 2019
+Timestamp: Spring 2020
 Filename: classify.py
 Goal: Classify land cover of various SEN12MS scenes
-Models used: ResNet v1, FC-DenseNet
+Models used:FC-DenseNet, Unet
 '''
 
 import warnings
@@ -73,7 +73,7 @@ def save_segmentation_predictions_on_scene_dir(model, scene_dir, save_dir, label
         else:
             path = os.path.join(save_dir, 'patch_{}.npz'.format(patch_id))
             np.savez_compressed(path, pred)
-    print('saved fc-densenet predictions to {}'.format(save_dir))
+    print('saved segmentation predictions to {}'.format(save_dir))
     return predictions
 
 def predict_model_path_on_each_scene(model_path, label_encoder, config):
@@ -83,13 +83,14 @@ def predict_model_path_on_each_scene(model_path, label_encoder, config):
     '''
     # load model from model_path
     if 'weights' in model_path and 'resnet' in model_path:
-        model = models.get_compiled_resnet(config, label_encoder)
-        model.load_weights(model_path)
-        mode = 'subpatches'
+        print('WARNING - resnet models have been deprecated!')
+        return
     elif 'weights' in model_path and 'DenseNet' in model_path:
         model = models.get_compiled_fc_densenet(config, label_encoder)
         model.load_weights(model_path)
-        mode = 'segmentation'
+    elif 'weights' in model_path and 'Unet' in model_path:
+        model = models.get_compiled_unet(config, label_encoder, predict_logits=True)
+        model.load_weights(model_path)
     else:
         print('ERROR: unable to load weights file!')
         return
@@ -99,21 +100,13 @@ def predict_model_path_on_each_scene(model_path, label_encoder, config):
     for continent in config['all_continents']:
         for season in config['all_seasons']:
             # get all scenes from this continent-season
-            scene_dirs = land_cover_utils.get_scene_dirs_for_continent_season(continent, season, config, mode)
-            if mode == 'subpatches':
-                # predict in subpatch/classification mode
-                for scene_dir in scene_dirs:
-                    scene_name = scene_dir.split('/')[-1]
-                    save_dir = os.path.join(config['subpatches_predictions_dir'],
-                        folder, model_name, '{}-{}'.format(continent, season), scene_name)
-                    save_resnet_predictions_on_scene_dir(model, scene_dir, save_dir, label_encoder, config)
-            else:
-                # predict in segmentation mode
-                for scene_dir in scene_dirs:
-                    scene_name = scene_dir.split('/')[-1]
-                    save_dir = os.path.join(config['segmentation_predictions_dir'],
-                        folder, model_name, '{}-{}'.format(continent, season), scene_name)
-                    save_segmentation_predictions_on_scene_dir(model, scene_dir, save_dir, label_encoder, config)
+            scene_dirs = land_cover_utils.get_scene_dirs_for_continent_season(continent, season, config)
+            # predict in segmentation mode
+            for scene_dir in scene_dirs:
+                scene_name = scene_dir.split('/')[-1]
+                save_dir = os.path.join(config['segmentation_predictions_dir'],
+                    folder, model_name, '{}-{}'.format(continent, season), scene_name)
+                save_segmentation_predictions_on_scene_dir(model, scene_dir, save_dir, label_encoder, config)
     print('finished predictions using model_path: ', model_path)
     print()
 
@@ -125,7 +118,7 @@ def predict_saved_models_on_each_scene(config):
     # get all saved models
     model_filepaths = glob.glob(os.path.join(config['model_save_dir'], '**/*.h5'))
     label_encoder = land_cover_utils.get_label_encoder(config)
-    # evaluate each saved model on each seasons/scene
+    # get predictions of each saved model on each seasons/scene
     for model_path in model_filepaths:
         print('Predicting using model path: ', model_path)
         predict_model_path_on_each_scene(model_path, label_encoder, config)
@@ -137,7 +130,7 @@ def predict_model_path_on_validation_set(model_path, label_encoder, config):
     Save predictions on each scene in the Validation set
     '''
     if 'Unet' in model_path:
-        model = models.get_compiled_unet(config, label_encoder, predict_logits=False)
+        model = models.get_compiled_unet(config, label_encoder, predict_logits=True)
     else:
         model = models.get_compiled_fc_densenet(config, label_encoder)
     model.load_weights(model_path)
@@ -154,125 +147,6 @@ def predict_model_path_on_validation_set(model_path, label_encoder, config):
         save_segmentation_predictions_on_scene_dir(model, scene_dir, save_dir, label_encoder, config, competition_mode=True)
     print('finished predictions using model_path: ', model_path)
     print()
-
-def evaluate_on_single_scene(sen12ms, config, label_encoder, \
-    model=None, model_path=None, \
-    test_season=None, test_scene_id=None):
-    '''
-    Inputs: sen12ms, config, model or model_path, test_season, test_scene_id
-    Output: classification report for this scene
-    '''
-    assert model is not None or model_path is not None
-    # load model from model_path, if necessary
-    if model is None:
-        if 'weights' in model_path and 'resnet' in model_path:
-            model = models.get_compiled_resnet(config, label_encoder)
-            model.load_weights(model_path)
-        elif 'weights' in model_path and 'DenseNet' in model_path:
-            model = models.get_compiled_fc_densenet(config, label_encoder)
-            model.load_weights(model_path)
-        else:
-            model = load_model(model_path)
-    # load data
-    s1, s2, lc, bounds = sen12ms.get_triplets(test_season, test_scene_id, \
-        s2_bands=config['s2_input_bands'])
-    patch_ids = sen12ms.get_patch_ids(test_season, test_scene_id)
-    # preprocessing: get subpatches, majority landuse class, etc.
-    if (model is not None and 'densenet' in model.name) or \
-            (model_path is not None and 'DenseNet' in model_path):
-        X_test, y_test, patch_ids = preprocess_s2_lc_for_segmentation(s2, lc, config, label_encoder, patch_ids)
-    else:
-        X_test, y_test, patch_ids = preprocess_s2_lc_for_classification(s2, lc, config, label_encoder, patch_ids)
-    # run evaluation
-    results = model.evaluate(X_test, y_test, verbose=1)
-    predictions = model.predict(X_test)
-    return results
-
-def evaluate_on_multiple_scenes(sen12ms, config, label_encoder, \
-    model=None, model_path=None, \
-    test_seasons=None, test_scene_ids=None, \
-    results_path=None):
-    '''
-    Inputs: sen12ms, config, model or model_path,
-        test_seasons (list of length S), test_scene_ids (list of length S),
-        results_path (optional)
-    Output: classification report for each scene (dict),
-        and save results to results_path (on disk)
-    '''
-    # input checks
-    assert model is not None or model_path is not None
-    assert len(test_seasons) == len(test_scene_ids)
-    # load model if necessary
-    label_encoder = land_cover_utils.get_label_encoder(config)
-    if model is None:
-        if 'weights' in model_path and 'resnet' in model_path:
-            model = models.get_compiled_resnet(config, label_encoder)
-            model.load_weights(model_path)
-        elif 'weights' in model_path and 'DenseNet' in model_path:
-            model = models.get_compiled_fc_densenet(config, label_encoder)
-            model.load_weights(model_path)
-        else:
-            model = load_model(model_path)
-    # evaluate on each test scene
-    all_results = defaultdict(dict)
-    for i in range(len(test_scene_ids)):
-        season = test_seasons[i]
-        scene_id = test_scene_ids[i]
-        print('Evaluating on season: {}, scene: {}'.format(season, scene_id))
-        scene_results = evaluate_on_single_scene(sen12ms, config, label_encoder, \
-            model=model,  test_season=season, test_scene_id=scene_id)
-        all_results[season.value][scene_id] = scene_results
-        # save results after each scene
-        if results_path is not None:
-            with open(results_path, 'w') as f:
-                json.dump(all_results, f, indent=4)
-    return all_results
-
-def evaluate_saved_models_on_each_season(config):
-    '''
-    Inputs: config (dict)
-    Output: saved results dict for each trained model (on disk)
-    '''
-    sen12ms = SEN12MSDataset(config['dataset_dir'])
-    label_encoder = land_cover_utils.get_label_encoder(config)
-    # get all seasons/scenes
-    seasons = []
-    scenes = []
-    for season in ALL_SEASONS:
-        for scene_id in sen12ms.get_scene_ids(season):
-            seasons.append(season)
-            scenes.append(scene_id)
-    # get all saved models
-    model_filepaths = glob.glob(os.path.join(config['model_save_dir'], '**/*.h5'))
-    # evaluate each saved model on each seasons/scene
-    for model_path in model_filepaths:
-        # for now, skip single-scene models
-        if 'by_scene' in model_path:
-            continue
-        print('Evaluating model path: ', model_path)
-        # get results json dump filepath
-        model_name = os.path.basename(model_path)
-        results_name = model_name.split('_weights.h5')[0] + '_results.json'
-        results_path = os.path.join(config['results_dir'], results_name)
-        # check if results are already complete for this model
-        if os.path.exists(results_path):
-            with open(results_path, 'rb') as f:
-                existing_results = json.load(f, object_hook=land_cover_utils.json_keys_to_int)
-            print("Note: {} already exists!".format(results_path))
-            season_results_complete = [len(season_results) == len(sen12ms.get_scene_ids(season)) \
-                for season, season_results in existing_results.items()]
-            print("Seasons complete? {}".format(season_results_complete))
-            if all(season_results_complete):
-                print("{} already complete! Skipping model eval".format(results_path))
-                continue
-        # don't evaluate single-scene models
-        if 'scene' in model_name:
-            continue
-        # evaluate model
-        evaluate_on_multiple_scenes(sen12ms, config, label_encoder, \
-            model_path=model_path, \
-            test_seasons=seasons, test_scene_ids=scenes, \
-            results_path=results_path)
 
 def train_segmentation_model_on_scene_dirs(scene_dirs, weights_path, config, \
     predict_continents=False, predict_seasons=False, \
@@ -463,6 +337,7 @@ def main(args):
     config_json_path = args.config_path
     with open(config_json_path, 'r') as f:
         config = json.load(f, object_hook=land_cover_utils.json_keys_to_int)
+    label_encoder = land_cover_utils.get_label_encoder(config)
 
     # configure GPU
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -475,7 +350,6 @@ def main(args):
 
     # show summary of keras models
     if args.model_summary:
-        label_encoder = land_cover_utils.get_label_encoder(config)
         fc_densenet = models.get_compiled_fc_densenet(config, label_encoder, predict_seasons=True)
         print('---------- FC-DENSENET MODEL SUMMARY ----------')
         #print(fc_densenet.summary())
@@ -498,19 +372,13 @@ def main(args):
         # predict_saved_models_on_each_scene(config)
         competition_model_path = os.path.join(config['model_save_dir'], config['competition_model'])
         print(f'predicting on competition data with model {competition_model_path}')
-        label_encoder = land_cover_utils.get_label_encoder(config)
         print(f'label_encoder.classes_: {label_encoder.classes_}')
         predict_model_path_on_validation_set(competition_model_path, label_encoder, config)
-
-    # evaluate saved models on each season/scene
-    if args.test:
-        evaluate_saved_models_on_each_season(config)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train or test land-cover model(s)')
     parser.add_argument('-c', '--config', dest='config_path', help='config JSON path')
     parser.add_argument('--train', dest='train', action='store_true', help='train new models')
-    parser.add_argument('--test', dest='test', action='store_true', help='test saved models')
     parser.add_argument('--predict', dest='predict', action='store_true', help='predict using saved models')
     parser.add_argument('--model_summary', dest='model_summary', action='store_true', help='print model summaries')
     args = parser.parse_args()
